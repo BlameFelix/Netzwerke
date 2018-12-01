4e26f85e4a92105dfa0c9e68337cb4ec367456b0
BlameFelix <45299563+BlameFelix@users.noreply.github.com>
2018-12-01 19:02:53 +0100

    Update client.c

--- client.c
+++ client.c
@@ -8,24 +8,22 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+
 #define BUF_LEN 128
 
 // Something unexpected happened. Report error and terminate.
-void sysErr( char *msg, int exitCode )
-{
+void sysErr( char *msg, int exitCode ) {
 	fprintf( stderr,"%s\n\t%s\n", msg, strerror( errno ) );
 	exit( exitCode );
 }
 
 // Called with wrong arguments.
-void usage( char *argv0 )
-{
+void usage( char *argv0 ) {
 	printf( "usage : %s server-ip portnumber\n", argv0 );
 	exit( 0 );
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	//checking if a server-ip and a port have been supplied
 	if(argc<3){
 		usage(argv[0]);
@@ -34,7 +32,6 @@ int main(int argc, char **argv)
 	//initializing variables
 	struct sockaddr_in serv_addr;
 	char sendBuf[BUF_LEN];
-	//unsigned int len;
 
 	//creating TCP based socket
 	int sockfd = 0;
@@ -59,54 +56,22 @@ int main(int argc, char **argv)
 	}
 	//user input
 	printf("Enter String: ");
-	fgets(sendBuf,BUF_LEN-1,stdin);
+	if(fgets(sendBuf,BUF_LEN-1,stdin)==-1) {
+	    sysErr("Client Fault: input", -4);
+	}
 
 	//send message
 	if((send(sockfd, sendBuf, strlen(sendBuf),0))==-1){
-		sysErr("Client fault: send message", -4);
+		sysErr("Client fault: send message", -5);
 	}
-	//get thex responde
+	//get the responde
 	char recvBuf[BUF_LEN];
-	if(/*len = */recv(sockfd, recvBuf, BUF_LEN, 0)==-1){
-		sysErr("Client Fault: recive message", -5);
+	if(recv(sockfd, recvBuf, BUF_LEN, 0)==-1){
+		sysErr("Client Fault: recive message", -6);
 	}
 	//print the responde
 	write(STDOUT_FILENO, recvBuf, strlen(recvBuf));
 
 	close(sockfd);
 	return 0;
-}/*
-//----------------------old code-------------------------------------------------------------------------eof------------------------------------------------
-	int sockfd;
-	socklen_t addrLen = sizeof(struct sockaddr_in);
-	struct sockaddr_in serv_addr;
-	char sendBuff[BUF_LEN];
-
-	sendBuff[BUF_LEN-1] = 0;
-
-	// Check for right number of arguments
-	if ( argc < 3 ) usage( argv[0] );
-
-	// Fill struct addr with IP addr, port and protocol family
-	memset( &serv_addr, 0, addrLen );
-	if ( ( serv_addr.sin_addr.s_addr = inet_addr( argv[ 1 ] ) ) == INADDR_NONE ) {
-		sysErr( "Client Fault: NOT_AN_IP", -1 );
-	}
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons( (u_short)atoi( argv[ 2 ] ) );
-
-	// Create new socket based on the values from
-	if ( ( sockfd = socket( AF_INET, SOCK_DGRAM, 0 ) ) == -1 ) {
-		sysErr( "Client Fault: SOCKET", -2 );
-	}
-
-	printf("Enter String: ");
-	fgets(sendBuff,BUF_LEN-1,stdin);
-	printf("Sending     : %s\n", sendBuff);
-	if ( sendto( sockfd, sendBuff, strlen(sendBuff), 0, (struct sockaddr *) &serv_addr, addrLen ) ==  -1 ) {
-			sysErr( "Client Fault: SENDTO", -4 );
-	}
-
-	close( sockfd );
-	return 0;
-}*/
+}
